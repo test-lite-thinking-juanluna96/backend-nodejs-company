53e5ca58630a8482ddbabbd91fe3a8fda5b8c82f
Juan Luna <juandluna17@gmail.com>
2022-11-21 17:19:03 -0500

    feat: update company (needs JWT and Roles validation)

--- src/datasources/company.datasource.ts
+++ src/datasources/company.datasource.ts
@@ -50,6 +50,38 @@ class CompanyDynamo implements CompanyRepository {
     }
   }
 
+  public async update (id: string, company: Company): Promise<Company> {
+    // Update company by id
+    const params = {
+      TableName: process.env.COMPANIES_TABLE,
+      Key: {
+        id
+      },
+      UpdateExpression: 'set #name = :name, #address = :address, #nit = :nit, #phone = :phone',
+      ExpressionAttributeNames: {
+        '#name': 'name',
+        '#address': 'address',
+        '#nit': 'nit',
+        '#phone': 'phone'
+      },
+      ExpressionAttributeValues: {
+        ':name': company.name,
+        ':address': company.address,
+        ':nit': company.nit,
+        ':phone': company.phone
+      },
+      ReturnValues: 'ALL_NEW'
+    }
+
+    const companyForm = await this.get(id)
+
+    if (companyForm) {
+      const result = await dynamo.update(params).promise()
+
+      return result.Attributes
+    }
+  }
+
   private async get (id: string): Promise<Company> {
     const params = {
       TableName: process.env.COMPANIES_TABLE,
